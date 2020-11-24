509edaff8adc4c6081704a7d9db6168fc18a3089
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2020-11-24 14:41:35 +0000

    added additional fields to user and sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_113027) do
+ActiveRecord::Schema.define(version: 2020_11_24_142745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "diseases", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diseases_on_user_id"
+  end
+
+  create_table "symptom_checks", force: :cascade do |t|
+    t.integer "rating"
+    t.text "description"
+    t.bigint "symptom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["symptom_id"], name: "index_symptom_checks_on_symptom_id"
+    t.index ["user_id"], name: "index_symptom_checks_on_user_id"
+  end
+
+  create_table "symptoms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "disease_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["disease_id"], name: "index_symptoms_on_disease_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +50,15 @@ ActiveRecord::Schema.define(version: 2020_11_24_113027) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "doctor", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diseases", "users"
+  add_foreign_key "symptom_checks", "symptoms"
+  add_foreign_key "symptom_checks", "users"
+  add_foreign_key "symptoms", "diseases"
 end
