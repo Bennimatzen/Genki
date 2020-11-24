a4b212143d6c6971045cf2bda1b59f47274b69ec
Jose Benitez <pepebenitez12@gmail.com>
2020-11-24 17:19:31 +0000

    Added edit and update actions to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   resources :symptom_checks, only: [:new, :create]
   resources :diseases, only: [:new, :create] do
     resources :symptoms, only: [:new, :create]
