10828a740aed8e3eeb42531be89273e7ebc25522
Areeb Bhaila <areeb.bhaila@gmail.com>
2020-11-25 12:46:20 +0000

    user-sign-up complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_150646) do
+ActiveRecord::Schema.define(version: 2020_11_25_115912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,9 @@ ActiveRecord::Schema.define(version: 2020_11_24_150646) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "doctor", default: false, null: false
+    t.date "dob"
+    t.integer "nhs_number"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
