301d1f22d1b08d15a5e4ff642ecbd02641df22f4
Jose Benitez <pepebenitez12@gmail.com>
2020-11-25 18:08:14 +0000

    I added a test for the date of the last check in db, but it is still not perfect

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,10 @@ Disease.create!(user: User.all.sample, name: "Cystic Fibrosis")
 puts "Creating symptoms..."
 Symptom.create!(disease: Disease.all.sample, name: "Cough")
 
+puts "Creating checks..."
+SymptomCheck.create!(user: User.all.sample, symptom: Symptom.all.sample, rating: 5, description: "Feeling regular", created_at: (Date.today - 1.day))
+
 puts "Done! #{User.count} users created."
 puts "Done! #{Disease.count} diseases created."
 puts "Done! #{Symptom.count} symptoms created."
+puts "Done! #{SymptomCheck.count} symptom checks created."
