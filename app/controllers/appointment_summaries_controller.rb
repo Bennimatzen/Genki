fe53f39e7835541a23a326de78229ca123481065
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2020-11-26 17:15:02 +0000

    appointment summary model, controller and views

--- app/controllers/appointment_summaries_controller.rb
+++ app/controllers/appointment_summaries_controller.rb
@@ -1,9 +1,25 @@
 class AppointmentSummariesController < ApplicationController
 
 def new
+  @appointment = Appointment.find(params[:appointment_id])
+  @appointment_summary = AppointmentSummary.new(appointment_summary_params)
 end
 
 def create
+  @appointment = Appointment.find(params[:appointment_id])
+  @appointment_summary = AppointmentSummary.new(appointment_summary_params)
+  @appointment_summary.appointment = @appointment
+  if @appointment_summary.save
+    redirect_to appointment_summary_path(@appointment_summary)
+  else
+    render :new
+  end
+end
+
+private
+
+def appointment_summary_params
+  params.require(:appointment_summary).permit(:content, :plan)
 end
 
 end
