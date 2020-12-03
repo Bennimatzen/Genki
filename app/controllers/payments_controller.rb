28c688e9f0e10b2e74bd6b5b0eee4564917349a6
Areeb Bhaila <areeb.bhaila@gmail.com>
2020-12-03 12:13:16 +0000

    completed styling

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,7 @@
 class PaymentsController < ApplicationController
   def new
     @order = current_user.orders.where(state: 'pending').find(params[:order_id])
+    @pharmacy = Pharmacy.find(@order.pharmacy_id)
+    @prescription = Prescription.find(@order.prescription_id)
   end
 end
